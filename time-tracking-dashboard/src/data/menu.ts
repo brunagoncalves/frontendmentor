64217c6715b9cde8c0bc17482a50c8c40f6409ef
Bruna Goncalves <brunagoncalves.sjb@gmail.com>
2024-12-28 14:32:02 -0300

    remove project

--- time-tracking-dashboard/src/data/menu.ts
+++
@@ -1,7 +0,0 @@
-import type { MenuTypes } from "../types/menuTypes";
-
-export const menuItem: MenuTypes[] = [
-  { name: "Daily" },
-  { name: "Weekly" },
-  { name: "Monthly" },
-];
--- time-tracking-dashboard/src/types/timeframeTypes.ts
+++
@@ -1,5 +0,0 @@
-export interface TimeframeData {
-  title: string;
-  timeframes: TimeframeData;
-  activeTimeframe: string;
-}
